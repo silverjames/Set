55925a8cc42aa40304ab27ec4e112bf82fe85e06
Bernhard F. Kraft <bernhard@bfk-engineering.com>
2018-07-27 12:01:03 +0200

    activated custom drawing for the buttons.

--- Set/CardView.swift
+++ Set/CardView.swift
@@ -40,17 +40,54 @@ class CardView: UIView {
 
     }
 
-//    override func draw(_ rect: CGRect) {
-//    }
+    override func draw(_ rect: CGRect) {
+
+        UIColor.green.setFill()
+        UIColor.blue.setStroke()
+        var shapeFunction: (CGRect) -> UIBezierPath
+
+        for idx in 0 ..< delegate!.getDealtCards().count {
+            shapeFunction = {
+                switch self.delegate!.getDealtCards()[idx].decoration[1] {
+                case 0:
+                    return self.createCircle($0)
+                case 1:
+                    return self.createDiamond($0)
+                case 2:
+                    return self.createOval($0)
+                default:
+                    return self.createCircle($0)
+                }
+            }//shape function closure
+ 
+            let button = UIButton()
+            button.frame = grid[idx]!.inset(by: buttonInset)
+            button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            button.isHidden = false
+
+            let shapeToDraw = shapeFunction(button.frame)
+            shapeToDraw.stroke()
+            shapeToDraw.fill()
+
+            button.addTarget(delegate, action: Selector(("touchCard:")), for: .touchUpInside)
+            self.addSubview(button)
+            gameButtons.append(button)
+
+//        let cages = Grid.init(layout:.dimensions(rowCount: getDimensionsForCell(rect).rowCount, columnCount: getDimensionsForCell(rect).columnCount) , frame: rect)
+//        for idx in 0..<cages.cellCount{
+//            let shapeToDraw = shapeFunction(cages[idx]!)
+//            shapeToDraw.stroke()
+//            shapeToDraw.fill()
+//        }
+
+        }//for loop: dealt cards
+    }//func
 
     //    *****************
     //    MARK Functions
     //    *****************
-//    @objc func touchCard(sender:UIButton){
-//        print("button pressed!")
-//    }
     
-    func updateViewFromModel(){
+    private func updateViewFromModel(){
         
         self.grid = Grid(layout: .aspectRatio(delegate!.getGridDimensions().aspectRatio))
         grid.cellCount = (delegate!.getGridDimensions().cellCount)
@@ -60,17 +97,7 @@ class CardView: UIView {
         }
         gameButtons.removeAll()
         grid.frame = self.bounds
-        
-        for idx in 0 ..< delegate!.getDealtCards().count {
-            let button = UIButton()
-            button.setTitle("😀", for: .normal)
-            button.frame = grid[idx]!.inset(by: buttonInset)
-            button.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
-            button.isHidden = false
-            button.addTarget(delegate, action: Selector(("touchCard:")), for: .touchUpInside)
-            self.addSubview(button)
-            gameButtons.append(button)
-        }
+//        self.setNeedsDisplay()
 
     }
     
@@ -89,9 +116,70 @@ class CardView: UIView {
         button.layer.borderColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
 //        button.setBackgroundImage(UIImage(named: "white")!, for: UIControl.State .normal)
     }
+    
+    private func createDiamond(_ rect: CGRect) -> UIBezierPath{
+        let diamond = UIBezierPath()
+        let axis = (min(rect.width, rect.height) * 0.9)/2
+        let center = getCenter(rect)
+        let p1 = CGPoint(x: center.x, y: center.y - axis)
+        let p2 = CGPoint(x: center.x + axis, y: center.y)
+        let p3 = CGPoint(x: center.x, y: center.y + axis)
+        let p4 = CGPoint(x: center.x - axis, y: center.y)
+        diamond.move(to: p1)
+        diamond.addLine(to: p2)
+        diamond.addLine(to: p3)
+        diamond.addLine(to: p4)
+        diamond.close()
+        return diamond
+    }
+    
+    private func createOval(_ rect: CGRect) -> UIBezierPath{
+        let oval = UIBezierPath()
+        let p1 = CGPoint(x: rect.origin.x + 0.75 * rect.size.width, y: rect.origin.y + 2*cardInset(rect))
+        let p2 = CGPoint(x: p1.x, y: rect.origin.y + rect.height - 2*cardInset(rect))
+        let p3 = CGPoint(x: rect.origin.x + 0.25 * rect.size.width, y: p2.y)
+        let p4 = CGPoint(x: p3.x, y:p1.y)
+        let cp1 = CGPoint(x: rect.origin.x + rect.size.width - cardInset(rect)/2, y: rect.origin.y + rect.height/2)
+        let cp2 = CGPoint(x: rect.origin.x + rect.size.width/2, y: rect.origin.y + rect.size.height - cardInset(rect)/2)
+        let cp3 = CGPoint(x: rect.origin.x + cardInset(rect)/2, y: cp1.y)
+        let cp4 = CGPoint(x: cp2.x, y: rect.origin.y + cardInset(rect)/2)
+        oval.move(to: p1)
+        oval.addQuadCurve(to: p2, controlPoint: cp1)
+        oval.addQuadCurve(to: p3, controlPoint: cp2)
+        oval.addQuadCurve(to: p4, controlPoint: cp3)
+        oval.addQuadCurve(to: p1, controlPoint: cp4)
+        oval.lineJoinStyle = .round
+        return oval
+    }
+    
+    private func createCircle(_ rect: CGRect) -> UIBezierPath{
+        let circle = UIBezierPath()
+        let center = getCenter(rect)
+        let radius = min(rect.width, rect.height)/2 * 0.9
+        circle.addArc(withCenter: center, radius: radius, startAngle: 0.0, endAngle: 2*CGFloat.pi, clockwise: false)
+        return circle
+    }
 
 }
 
 private struct Constants{
     static let insets = CGFloat(6.0)
+}
+
+extension CardView{
+    private struct SetViewRatios {
+        static let frameInsetRatio: CGFloat = 0.08
+        static let maxSymbolsPerCard = 3
+    }
+    
+    private func cardInset (_ rect: CGRect) -> CGFloat{
+        return (rect.width + rect.height)/2 * SetViewRatios.frameInsetRatio
+    }
+    private func symbolBounds (_ bounds: CGRect) -> CGRect{
+        let symbolFrame = bounds.inset(by: UIEdgeInsets.init(top: cardInset(bounds), left: cardInset(bounds), bottom: cardInset(bounds), right: cardInset(bounds)))
+        return symbolFrame
+    }
+    private func getCenter (_ rect:CGRect) -> CGPoint{
+        return CGPoint(x: rect.midX, y: rect.midY)
+    }
 }
